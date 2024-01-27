bf7ae2c9b03a8db53be8052dbf9be624c04fa640
arafat-mahmud <arafat.mahmud.2001@gmail.com>
2024-01-28 00:01:34 +0600

    Concatenation & interpolation

---
+++ String_Concatenation_&_interpolation.dart
@@ -0,0 +1,19 @@
+void main(){
+  String a = 'We';
+  String b = 'are'; 
+  String c = 'learning';
+  String d = 'Dart';
+
+  // String Literals
+  String e = 'We' 'are' 'learning' 'Dart';
+
+  // Step 1
+  print(a + b + c + d);
+
+  //Step 2
+  print("$a $b $c $d");
+
+  // step 3
+  print(e);
+
+}
