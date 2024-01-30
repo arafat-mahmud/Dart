08e0d4338ffef1af92b072da524844a4d58f8029
arafat-mahmud <arafat.mahmud.2001@gmail.com>
2024-01-30 21:16:49 +0600

    Class

---
+++ 13. Class.dart
@@ -0,0 +1,11 @@
+import '13.1. Example.dart';
+
+void main() {
+  var obj = Example();
+  print(obj.name);
+  print(obj.age);
+  obj.myFunction();
+
+  print(obj.ageTwo);
+  obj.myFunctionTwo();
+}
---
+++ 13.1. Example.dart
@@ -0,0 +1,12 @@
+class Example {
+  int age = 23;
+  String name = 'Arafat  Mahmud';
+  myFunction() {
+    print("My function");
+  }
+  
+  int ageTwo = 20;
+  myFunctionTwo() {
+    print("My function two");
+  }
+}
