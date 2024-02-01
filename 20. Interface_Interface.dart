94c2bed3152890d209e6093edf60f161af407521
arafat-mahmud <arafat.mahmud.2001@gmail.com>
2024-02-01 21:21:08 +0600

    Interface_Interface

---
+++ 20. Interface_Interface.dart
@@ -0,0 +1,30 @@
+void main(){
+
+
+
+}
+
+class A{
+  methodOne(){
+    print('Method One');
+
+  }
+
+  methodTwo(){
+    print('Method Two');
+
+  }
+}
+
+class B implements A{
+  @override
+  methodOne() {
+    
+  }
+
+  @override
+  methodTwo() {
+    
+  }
+  
+}
